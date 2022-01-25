933176769d4c64f1f2878807a6586ff46437bb5c
Brian Chen <chenbm4@gmail.com>
2022-01-25 02:45:30 -0800

    initialized developer guides

--- python-websockets/client.py
+++ python-websockets/client.py
@@ -7,8 +7,9 @@ async def listen():
     url = "ws://sim.smogon.com:8000/showdown/websocket"
 
     async with websockets.connect(url) as ws:
-        msg = await ws.recv()
-        print(msg)
+        while True:
+            msg = await ws.recv()
+            print(msg)
 
 # runs function listen asynchronously until everything is complete
 asyncio.get_event_loop().run_until_complete(listen())
