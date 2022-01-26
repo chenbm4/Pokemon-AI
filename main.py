9d105e7cc64fdbb863fd5063bca8978f25cd85f5
chenbm4 <chenbm4@gmail.com>
2022-01-25 21:22:08 -0800

    login functionality completed and working

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from python_websockets.client import WebsocketClient
 
 async def pokemon_ai(username, password): # move username and password to config file later
     websocket_client = await WebsocketClient.create(username, password)
-    await websocket_client.listen()
+    await websocket_client.login()
+
+    while True:
+        await websocket_client.listen()
 
 # runs function pokemon_ai asynchronously until everything is complete
-asyncio.get_event_loop().run_until_complete(pokemon_ai("hehe", "xd"))
+asyncio.get_event_loop().run_until_complete(pokemon_ai("obri1", "hellocse115"))
--- python_websockets/client.py
+++ python_websockets/client.py
@@ -1,6 +1,11 @@
 
 import websockets
 import asyncio
+import requests
+import json
+
+class LoginError(Exception):
+    pass
 
 # the WebsocketClient class we will be using to interface with PS servers
 class WebsocketClient:
@@ -19,7 +24,7 @@ class WebsocketClient:
         self.password = password
         self.address = "ws://sim.smogon.com:8000/showdown/websocket"
         self.websocket = await websockets.connect(self.address)
-        self.login_uri = "https://play.pokemonshowdown.com/action.php"   # not sure what this is for yet
+        self.login_uri = "https://play.pokemonshowdown.com/action.php"
         return self
 
     # receives and prints message from server
@@ -36,3 +41,51 @@ class WebsocketClient:
         print(formatted_message)
         await self.websocket.send(formatted_message)
         self.last_message = formatted_message
+
+    # PS servers will send the following string to the client:
+    #   "|challstr|(client ID)|(challstr)"
+    # In order to log in, we need both client ID and challstr, so this function returns both
+    async def get_id_and_challstr(self):
+        while True:
+            message = await self.listen()
+            split_message = message.split('|')
+            if split_message[1] == 'challstr':
+                return split_message[2], split_message[3]
+
+    # attempts to log the user in using the stored username and password
+    async def login(self):
+        print("Logging in as " + self.username)
+        client_id, challstr = await self.get_id_and_challstr()
+
+        # according to PS github, must make an HTTP POST request to the login_uri
+        # with the data act=login&name=USERNAME&pass=PASSWORD&challstr=CHALLSTR
+        # requests.post essentially does that for us
+        response = requests.post(
+            self.login_uri,
+            data={
+                'act': 'login',
+                'name': self.username,
+                'pass': self.password,
+                'challstr': "|".join([client_id, challstr])
+            }
+        )
+
+        # status code 200 means the HTTP request has gone through successfully
+        if response.status_code == 200:
+            # json helps to format the response text in an easily-parsable way for us to use
+            response_json = json.loads(response.text[1:])
+
+            if not response_json['actionsuccess']:
+                print("Login Unsuccessful")
+                raise LoginError("Could not log-in")
+
+            # to finish logging in, must send /trn USERNAME,0,ASSERTION where USERNAME is your desired username 
+            # and ASSERTION is data.assertion.
+
+            assertion = response_json.get('assertion')
+            message = ["/trn " + self.username + ",0," + assertion]
+            print("Successfully logged in")
+            await self.send('', message)
+        else:
+            print.error("Could not log-in\nDetails:\n{}".format(response.content))
+            raise LoginError("Could not log-in")
