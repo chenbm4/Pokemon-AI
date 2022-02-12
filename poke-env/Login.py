1845ef22ab31d3ac8336e6176cb02df3cf7ed4a1
Carter Welke <carterwelke@gmail.com>
2022-02-11 22:31:59 -0800

    RandomBot logs in to showdown as CarterBattleBot and will accept 1 challenge from any account

---
+++ poke-env/Login.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from poke_env.player.random_player import RandomPlayer
+from poke_env.player_configuration import PlayerConfiguration
+from poke_env.server_configuration import ShowdownServerConfiguration
+
+async def main():
+    # We create a random player
+    #logs in as user: 'CarterBattleBot'
+    player = RandomPlayer(
+        player_configuration=PlayerConfiguration("CarterBattleBot", "PacifidlogTown"),
+        server_configuration=ShowdownServerConfiguration,
+    )
+    
+    # Sending challenges to 'your_username'
+    # Not working on official pokemon showdown servers
+    # await player.send_challenges("TheMagnaCarta", n_challenges=1)
+
+    # Accepting one challenge from any user
+    # send challenge to bot under 'find a user' button and type in bot's username
+    await player.accept_challenges(None, 1)
+
+    # Accepting three challenges from 'your_username'
+    # await player.accept_challenges('your_username', 3)
+
+    # Playing 5 games on the ladder
+    # UNTESTED
+    # await player.ladder(5)
+
+    # Print the rating of the player and its opponent after each battle
+    for battle in player.battles.values():
+        print(battle.rating, battle.opponent_rating)
+
+    # terminates after all battles are finished
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
