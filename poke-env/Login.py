9ce1680d9f5cf4b871c3deea4ebfab42d1095b82
Antonin Laurent <alaurent22@github.com>
2022-02-19 00:22:51 -0800

    Login.py now polls the user for various options

--- poke-env/Login.py
+++ poke-env/Login.py
@@ -1,17 +1,73 @@
 import asyncio
+import os
 
 from poke_env.player_configuration import PlayerConfiguration
 from poke_env.server_configuration import ShowdownServerConfiguration
 
+from poke_env.player.random_player import RandomPlayer
 from pokebot import MaxDamagePlayer
 
+
+# This is a simple helper function to handle user inputs forgivingly.
+# This takes an iterable and returns an index that the user has chosen.
+# -1 is never returned
+def input_options(options):
+    result = -1
+    while (result not in range(len(options))): # Loop until result is good
+        print("Select one of the following options:")
+        for i in range(len(options)):
+            print(str(i)+": " + str(options[i]))
+        try:
+            result = int(input("> "))
+        except ValueError:
+            print("Please enter a number")
+            result = -1
+    return result
+
+# This is how the user selects 
+#   which bot to use, 
+#   what account to log into, 
+#   which team to use, 
+#   what battle format
+#   what server
+# returns tuple of bot, accountinfo, team, format, server object
+# TODO move account information somewhere else for security
+# could implement ini reading here
+def bot_selector():
+    bot_options = (RandomPlayer, MaxDamagePlayer)
+    bot_options_strings = ("RandomPlayer", "MaxDamagePlayer")
+    bot_index = input_options(bot_options_strings)
+    bot = bot_options[bot_index]
+    
+    account_options = (("CarterBattleBot", "PacifidlogTown"), ("Random Bot", None))
+    account_options_strings = ("CarterBattleBot", "Random Bot")
+    account_index = input_options(account_options_strings)
+    account = account_options[account_index]
+	
+    # TODO use os.listdir to get files in Teams folder, then run them through input_options
+    # currently untestable since no extant bots use teams
+    team = None
+    
+    bformat_options = ("gen8randombattle")
+    bformat_options_strings = ("Gen 8 Random Battle",)
+    bformat_index = input_options(bformat_options_strings)
+    bformat = bformat_options[bformat_index]
+    
+    server_options = (ShowdownServerConfiguration, None)
+    server_options_strings = ("Showdown Server", "Local Server")
+    server_index = input_options(server_options_strings)
+    server = server_options[server_index]
+
+    return (bot, account, team, bformat, server)
+
 async def main():
+    selected = bot_selector()
+
     # We create a random player
-    #logs in as user: 'CarterBattleBot'
-    player = MaxDamagePlayer(
-        player_configuration=PlayerConfiguration("CarterBattleBot", "PacifidlogTown"),
-        battle_format="gen8randombattle",
-        server_configuration=ShowdownServerConfiguration,
+    player = selected[0](
+        player_configuration=PlayerConfiguration(selected[1][0], selected[1][1]),
+        battle_format=selected[3],
+        server_configuration=selected[4],
     )
     
     # Sending challenges to 'your_username'
